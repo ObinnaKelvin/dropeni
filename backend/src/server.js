f59deeeebae9f801a29420f0779da050feb3a93d
Obinna Okere <okereobinna10@gmail.com>
2026-03-10 23:41:09 +0100

    backend/server.js adjusted

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,5 +23,5 @@ if (ENV.NODE_ENV === "production") {
 }
 
 app.listen(ENV.PORT, () => {
-    console.log("Server is running on port 3000")
+    console.log(`Server is running on port ${ENV.PORT}`)
 })
