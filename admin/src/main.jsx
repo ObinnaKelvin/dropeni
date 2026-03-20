f2fd7fd7a438fdbf165909fde9909750aee1f735
Obinna Okere <okereobinna10@gmail.com>
2026-03-20 18:40:12 +0100

    packages updated globally

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react'
+
+//Import Clerk Publishable Key from environment variables
+const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!clerkPublishableKey) {
+  console.error('❌❌Clerk Publishable Key is missing. Please set VITE_CLERK_PUBLISHABLE_KEY in your environment variables.');
+  throw new Error('Clerk Publishable Key is required');
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerkPublishableKey}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
